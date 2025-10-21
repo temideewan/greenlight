d5f166581822594c738317fdc361bbcb3d8f68a4
Temidayo Omoyajowo <temidayoomoyajowo@seamlesshr.com>
2025-10-21 21:43:56 +0100

    feat: handle json responses and errors

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,10 +10,15 @@ func (app *application) routes() http.Handler {
 	// initialize a new http router instance
 	router := httprouter.New()
 
-	// register endpoints
+	// use custom not found handler for the httprouter notfound
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 
+	// use custom method not allowed handler for the httprouter method not allowed
+	router.MethodNotAllowed = http.HandlerFunc(app.notAllowedResponse)
+
+	// register endpoints
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
-	return router
+	return app.recoverPanic(router)
 }
