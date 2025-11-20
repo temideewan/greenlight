eba6205e68f1f59a3a573dc0b54c72d7828eb57e
Temidayo Omoyajowo <temidayoomoyajowo@seamlesshr.com>
2025-11-20 19:54:24 +0100

    feat: validation for users

--- internal/data/users.go
+++ internal/data/users.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"golang.org/x/crypto/bcrypt"
+	"greenlight.temideewan.net/internal/validator"
 )
 
 type User struct {
@@ -49,4 +50,30 @@ func (p *password) Matches(plaintextPassword string) (bool, error) {
 	}
 	return true, nil
 
+}
+
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be provided")
+	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
+}
+
+func ValidatePassword(v *validator.Validator, password string) {
+	v.Check(password != "", "password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
+	v.Check(len(password) <= 72, "password", "must not be more than 72 bytes long")
+}
+
+func ValidateUser(v *validator.Validator, user *User) {
+	v.Check(user.Name != "", "name", "must be provided")
+	v.Check(len(user.Name) <= 500, "name", "must not be more than 500 bytes long")
+
+	ValidateEmail(v, user.Email)
+
+	if user.Password.plaintext != nil {
+		ValidatePassword(v, *user.Password.plaintext)
+	}
+
+	if user.Password.hash == nil {
+		panic("missing password hash for user")
+	}
 }
