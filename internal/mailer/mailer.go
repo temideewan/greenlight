b969c2eb8c0542a76f5198af1bc200d3a3da0fee
Temidayo Omoyajowo <temidayoomoyajowo@seamlesshr.com>
2025-11-25 08:37:07 +0100

    feat: setup email helpers

---
+++ internal/mailer/mailer.go
@@ -0,0 +1,106 @@
+package mailer
+
+import (
+	"bytes"
+	"embed"
+	"time"
+
+	"github.com/wneessen/go-mail"
+
+	// Import the html/template and text/template packages. Because these share the same
+	// package name ("template") we need disambiguate them and alias them to ht and tt
+	// respectively.
+	ht "html/template"
+	tt "text/template"
+)
+
+// Below we declare a new variable with the type embed.FS (embedded file system) to hold
+// our email templates. This has a comment directive in the format `//go:embed <path>`
+// IMMEDIATELY ABOVE it, which indicates to Go that we want to store the contents of the
+// ./templates directory in the templateFS embedded file system variable.
+// ↓↓↓
+
+//go:embed "templates"
+var templateFS embed.FS
+
+type Mailer struct {
+	client *mail.Client
+	sender string
+}
+
+func New(host string, port int, username, password, sender string) (*Mailer, error) {
+
+	client, err := mail.NewClient(
+		host,
+		mail.WithSMTPAuth(mail.SMTPAuthLogin),
+		mail.WithPort(port),
+		mail.WithUsername(username),
+		mail.WithPassword(password),
+		mail.WithTimeout(5*time.Second),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	mailer := &Mailer{
+		client: client,
+		sender: sender,
+	}
+	return mailer, nil
+}
+
+func (m *Mailer) Send(recipient string, templateFile string, data any) error {
+	// Use the ParseFS() method from text/template to parse the required template file
+	// from the embedded file system.
+	textTmpl, err := tt.New("").ParseFS(templateFS, "templates/"+templateFile)
+
+	if err != nil {
+		return err
+	}
+
+	// Execute the named template "subject", passing in the dynamic data and storing the
+	// result in a bytes.Buffer variable.
+	subject := new(bytes.Buffer)
+	err = textTmpl.ExecuteTemplate(subject, "subject", data)
+
+	if err != nil {
+		return err
+	}
+
+	plainBody := new(bytes.Buffer)
+	err = textTmpl.ExecuteTemplate(plainBody, "plainBody", data)
+
+	if err != nil {
+		return err
+	}
+
+	htmlTmpl, err := ht.New("").ParseFS(templateFS, "templates/"+templateFile)
+	if err != nil {
+		return err
+	}
+
+	htmlBody := new(bytes.Buffer)
+	err = htmlTmpl.ExecuteTemplate(htmlBody, "htmlBody", data)
+	if err != nil {
+		return err
+	}
+
+	// Use the mail.NewMsg() function to initialize a new mail.Msg instance.
+	// Then we use the To(), From() and Subject() methods to set the email recipient,
+	// sender and subject headers, the SetBodyString() method to set the plain-text body,
+	// and the AddAlternativeString() method to set the HTML body.
+	msg := mail.NewMsg()
+
+	err = msg.From(m.sender)
+
+	if err != nil {
+		return err
+	}
+
+	msg.Subject(subject.String())
+	msg.SetBodyString(mail.TypeTextPlain, plainBody.String())
+	msg.AddAlternativeString(mail.TypeTextHTML, htmlBody.String())
+
+	return m.client.DialAndSend(msg)
+}
